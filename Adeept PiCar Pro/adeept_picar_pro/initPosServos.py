6dacd2acb70b511d0f5731e0d941cfb7c0152571
bbueno5000 <bbueno5000@outlook.com>
2023-04-27 21:15:59 -0600

    format

--- Adeept PiCar Pro/adeept_picar_pro/initPosServos.py
+++ Adeept PiCar Pro/adeept_picar_pro/initPosServos.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
+
 # File name   : init
 # Description : Control Servos
 # Author      : William
 # Date        : 2019/02/23
-import time
+
 import Adafruit_PCA9685
+import time
 
 pwm = Adafruit_PCA9685.PCA9685()
 pwm.set_pwm_freq(50)
