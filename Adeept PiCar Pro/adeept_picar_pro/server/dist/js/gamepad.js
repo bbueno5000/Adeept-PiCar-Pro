8ed64e0f28c34224be9d4f86c4d17264c9659c0e
bbueno5000 <bbueno5000@outlook.com>
2023-05-15 13:42:18 -0600

    update

--- Adeept PiCar Pro/adeept_picar_pro/server/dist/js/gamepad.js
+++ Adeept PiCar Pro/adeept_picar_pro/server/dist/js/gamepad.js
@@ -13,6 +13,18 @@ function buttonPressed(b) {
     return b === 1.0;
 }
 
+function pollGamepads() {
+
+    const gamepads = navigator.getGamepads();
+    for (const gamepad of gamepads) {
+        gamepadInfo.textContent = "Gamepad connected at index ${gamepad.index}: ${gamepad.id}.";
+        var t = "ws://" + location.hostname + ":8888/echo";
+        const websocket = new WebSocket(t);
+        websocket.onopen = () => websocket.send("admin:123456");
+        setInterval(sendMoves(websocket, gamepad), 100);
+    }
+}
+
 /**
  * 
  * @param {any} websocket
@@ -21,10 +33,12 @@ function sendMoves(websocket, gamepad) {
 
     let command;
     if (buttonPressed(gamepad.buttons[0])) {
-        command = 'Switch_3_on';
+        console.log("up");
+        command = "Switch_3_on";
     }
     else if (buttonPressed(gamepad.buttons[1])) {
-        command = 'Switch_3_off';
+        console.log("down");
+        command = "Switch_3_off";
     }
     websocket.send(JSON.stringify(command));
 }
@@ -34,11 +48,8 @@ function sendMoves(websocket, gamepad) {
  */
 function start() {
 
-    var t = "ws://" + location.hostname + ":8888/echo";
-    const websocket = new WebSocket(t);
-    websocket.onopen = () => websocket.send("admin:123456");
-    window.addEventListener("gamepadconnected", (event) => {
-        setInterval(sendMoves(websocket, event.gamepad), 100);
+    window.addEventListener("gamepadconnected", (e) => {
+        setInterval(pollGamepads, 500);
     });
 }
 
