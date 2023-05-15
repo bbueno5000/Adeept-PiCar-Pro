e8c365ad9e966e66d6d5d98d84ed141a5bc2235d
bbueno5000 <bbueno5000@outlook.com>
2023-05-15 12:50:51 -0600

    update

--- Adeept PiCar Pro/adeept_picar_pro/server/dist/js/gamepad.js
+++ Adeept PiCar Pro/adeept_picar_pro/server/dist/js/gamepad.js
@@ -17,13 +17,12 @@ function buttonPressed(b) {
  * 
  * @param {any} event
  */
-function connectHandler(event, websocket) {
+function connectHandler(event) {
 
     console.log(
         "Gamepad connected at index %d: %s",
         event.gamepad.index, event.gamepad.id
     );
-    sendMoves(websocket, event.gamepad);
 }
 
 /**
@@ -34,11 +33,9 @@ function sendMoves(websocket, gamepad) {
 
     let command;
     if (buttonPressed(gamepad.buttons[0])) {
-        console.log('up');
         command = 'Switch_3_on';
     }
     else if (buttonPressed(gamepad.buttons[1])) {
-        console.log('down');
         command = 'Switch_3_off';
     }
     websocket.send(JSON.stringify(command));
@@ -52,13 +49,11 @@ function start() {
     var t = "ws://" + location.hostname + ":8888/echo";
     const websocket = new WebSocket(t);
     websocket.onopen = () => websocket.send("admin:123456");
+    window.addEventListener("gamepadconnected", connecthandler);
     const gamepad = navigator.getGamepads()[0];
     if ("ongamepadconnected" in window) {
         setInterval(sendMoves(websocket, gamepad), 100);
     }
-//    window.addEventListener("gamepadconnected", (e) => {
-//        connectHandler(e, websocket);
-//    }, false);
 }
 
 window.onload = start;
