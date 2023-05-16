e5daa127380c404494fef9c12d526ab19acee88f
bbueno5000 <bbueno5000@outlook.com>
2023-05-16 01:24:35 -0600

    update

--- Adeept PiCar Pro/adeept_picar_pro/server/dist/js/gamepad.js
+++ Adeept PiCar Pro/adeept_picar_pro/server/dist/js/gamepad.js
@@ -18,17 +18,24 @@ function buttonPressed(b) {
 
 /**
  * 
+ * @param {any} event
+ */
+function connectHandler(event) {
+
+    console.log(
+        "Gamepad connected at index %d: %s",
+        event.gamepad.index, event.gamepad.id
+    );
+    gamepad = event.gamepad;
+    interval = setInterval(sendMoves, 100);
+}
+
+/**
+ * 
  * @param {any} websocket
  * @param {any} gamepad
  */
-function sendMoves(gp) {
-
-    gamepad = gp;
-    interval = setInterval(update, 100)
-//    websocket.send(JSON.stringify(command));
-}
-
-function update() {
+function sendMoves() {
 
     let command;
     if (buttonPressed(gamepad.buttons[0])) {
@@ -40,6 +47,7 @@ function update() {
         command = "Switch_3_off";
     }
     clearInterval(interval);
+//    websocket.send(JSON.stringify(command));
 }
 
 /**
@@ -50,13 +58,7 @@ function start() {
 //    var t = "ws://" + location.hostname + ":8888/echo";
 //    const websocket = new WebSocket(t);
 //    websocket.onopen = () => websocket.send("admin:123456");
-    window.addEventListener("gamepadconnected", (e) => {
-        console.log(
-            "Gamepad connected at index %d: %s",
-            e.gamepad.index, e.gamepad.id
-        );
-        sendMoves(e.gamepad);
-    });
+    window.addEventListener("gamepadconnected", connectHandler);
 }
 
 window.onload = start;
